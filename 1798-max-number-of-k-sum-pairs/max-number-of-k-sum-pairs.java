571a12b0fb7ee78b0bc7cdf287f7539f7b474583
ManemDhanush <42865626+ManemDhanush@users.noreply.github.com>
2024-08-09 16:13:17 -0400

    Time: 36 ms (29.06%) | Memory: 55.7 MB (81.49%) - LeetSync

--- 1798-max-number-of-k-sum-pairs/max-number-of-k-sum-pairs.java
+++ 1798-max-number-of-k-sum-pairs/max-number-of-k-sum-pairs.java
@@ -1,27 +1,18 @@
 class Solution {
     public int maxOperations(int[] nums, int k) {
-        // Hashtable<Integer, Integer> hashtable = new Hashtable<>();
-        HashMap<Integer, Integer> hashtable = new HashMap<Integer, Integer>();
+        HashMap<Integer, Integer> hashtable = new HashMap<>();
         int ans = 0;
-        for(int num: nums){
-            hashtable.put(num, hashtable.getOrDefault(num, 0) + 1);
-        }
-
+        
         for(int num: nums){
             int target = k - num;
-            if(target == num && hashtable.getOrDefault(target, -9) > 1){
-                // System.out.println(hashtable.getOrDefault(target, -9));
-                hashtable.put(target, hashtable.get(target) - 2);
+            if(hashtable.getOrDefault(target, 0) > 0) {
                 ans++;
-            }
-            else if(target != num && hashtable.getOrDefault(target, -9) > 0 && hashtable.getOrDefault(num, -9) > 0){
                 hashtable.put(target, hashtable.get(target) - 1);
-                hashtable.put(num, hashtable.get(num) - 1);
-                ans++;
+            } else {
+                hashtable.put(num, hashtable.getOrDefault(num, 0) + 1);
             }
-            // System.out.println(hashtable.toString());
         }
-        // System.out.println(hashtable.toString());
+        
         return ans;
     }
 }

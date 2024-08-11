da19aeeaef0cd042527e333c4ef69219f8e70dbf
ManemDhanush <42865626+ManemDhanush@users.noreply.github.com>
2024-08-11 14:42:51 -0400

    Time: 2 ms (96.39%) | Memory: 57.2 MB (11.45%) - LeetSync

--- 1586-longest-subarray-of-1s-after-deleting-one-element/longest-subarray-of-1s-after-deleting-one-element.java
+++ 1586-longest-subarray-of-1s-after-deleting-one-element/longest-subarray-of-1s-after-deleting-one-element.java
@@ -1,32 +1,22 @@
 class Solution {
     public int longestSubarray(int[] nums) {
-        int count = 0;
+        int prevLength = 0;
+        int currentLength = 0;
         int maxCount = 0;
-        boolean skipped = false;
-        ArrayList<Integer> list = new ArrayList<>(); 
-        for(int i=0; i<nums.length; i++){
-            if(nums[i] == 1){
-                count++;
+        
+        for(int n: nums){
+            if(n == 1){
+                currentLength++;
             } else {
-                list.add(count);
-                count = 0;
-                list.add(count);
+                maxCount = Math.max(maxCount, currentLength + prevLength);
+                prevLength = currentLength;
+                currentLength = 0;
             }
         }
-        list.add(count);
-        // System.out.print(list);
-        int first = 0, second = 0, third = 0;
-        for(int i=0; i<list.size(); i++){
-            third = second;
-            second = first;
-            first = list.get(i);
 
-            maxCount = Math.max(maxCount, first + second + third);
-        }
-        if(maxCount == nums.length){
-            return maxCount - 1;
-        }
+        maxCount = Math.max(maxCount, currentLength + prevLength);
 
-        return maxCount;
+        return maxCount == nums.length ? maxCount - 1 : maxCount;
+
     }
 }

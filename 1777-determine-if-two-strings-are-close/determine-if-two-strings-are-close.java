b2c147f2a398ad7ad7f89bbe0514a511c1e588ee
ManemDhanush <42865626+ManemDhanush@users.noreply.github.com>
2024-08-13 14:36:36 -0400

    Time: 68 ms (34.15%) | Memory: 45.7 MB (24.75%) - LeetSync

---
+++ 1777-determine-if-two-strings-are-close/determine-if-two-strings-are-close.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean closeStrings(String word1, String word2) {
+        // Map<Character, Integer> map1 = word1.toCharArray().each(ch -> map1.put(ch, map1.getOrDefault(ch, 0) + 1));
+        Map<Character, Integer> map1 = new HashMap<>();
+        Map<Character, Integer> map2 = new HashMap<>();
+
+        for (char ch : word1.toCharArray()) {
+            map1.put(ch, map1.getOrDefault(ch, 0) + 1);
+        }
+
+        for (char ch : word2.toCharArray()) {
+            map2.put(ch, map2.getOrDefault(ch, 0) + 1);
+        }
+
+        List<Integer> list1 = map1.values().stream().sorted().collect(Collectors.toList());
+        List<Integer> list2 = map2.values().stream().sorted().collect(Collectors.toList());
+
+        List<Character> charList1 = map1.keySet().stream().sorted().collect(Collectors.toList());
+        List<Character> charList2 = map2.keySet().stream().sorted().collect(Collectors.toList());
+
+        return list1.equals(list2) && charList1.equals(charList2);
+    }
+}

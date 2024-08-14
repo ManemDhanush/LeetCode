bb7f34333e75f04e36659232988c36d300929f84
ManemDhanush <42865626+ManemDhanush@users.noreply.github.com>
2024-08-13 20:43:16 -0400

    Time: 5 ms (16.73%) | Memory: 42 MB (12.37%) - LeetSync

--- 394-decode-string/decode-string.java
+++ 394-decode-string/decode-string.java
@@ -3,27 +3,34 @@ class Solution {
 
 
         Stack<Character> stack = new Stack<>();
+        StringBuilder str = new StringBuilder();
+        StringBuilder num = new StringBuilder();
 
         for(char ch: s.toCharArray()){
             if(ch == ']'){
-                String str = "";
+                // String str = "";
                 while(!stack.isEmpty() && stack.peek() != '['){
-                    str = stack.pop() + str;
+                    // str = stack.pop() + str;
+                    str.insert(0,stack.pop());
                 }
                 stack.pop();
-                String num = "";
+                // String num = "";
                 while(!stack.isEmpty() && Character.isDigit(stack.peek())){
-                    num = stack.pop() + num;
+                    // num = stack.pop() + num;
+                    num.insert(0,stack.pop());
                 }
 
                 // System.out.println(str);
                 // System.out.println(num);
 
-                for(int i=0; i<Integer.parseInt(num); i++){
-                    for(char c: str.toCharArray()){
+                for(int i=0; i<Integer.parseInt(num.toString()); i++){
+                    for(char c: str.toString().toCharArray()){
                         stack.push(c);
                     }
                 }
+
+                num.setLength(0);
+                str.setLength(0);
             } else {
                 stack.push(ch);
             }

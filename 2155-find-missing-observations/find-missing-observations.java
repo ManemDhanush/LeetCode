038350006df72750f1cd3c536e1fbb575ffa373c
ManemDhanush <42865626+ManemDhanush@users.noreply.github.com>
2024-09-05 10:37:01 -0400

    Time: 8 ms (11.48%) | Memory: 59.2 MB (96.39%) - LeetSync

---
+++ 2155-find-missing-observations/find-missing-observations.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int[] missingRolls(int[] rolls, int mean, int n) {
+        
+        int sum = Arrays.stream(rolls).sum();
+
+        double avg = ((mean * (rolls.length + n)) - sum) * 1.0 / n;
+        int mod = ((mean * (rolls.length + n)) - sum) % n;
+        // System.out.println(avg);
+        if(avg > 6 || avg < 1) return new int[0];
+
+        int[] ans = new int[n];
+        for(int i=0; i<n; i++){
+            ans[i] = (int)avg;
+        }
+        for(int i=0; i<mod; i++){
+            ans[i]++;
+        }
+        
+        return ans;
+    }
+}

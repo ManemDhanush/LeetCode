d8c8899335e28cc653b1b0c0a9ceb767f3a7ab09
ManemDhanush <42865626+ManemDhanush@users.noreply.github.com>
2025-02-23 06:47:48 -0500

    Time: 0 ms (100.00%) | Memory: 43.1 MB (76.30%) - LeetSync

--- 747-min-cost-climbing-stairs/min-cost-climbing-stairs.java
+++ 747-min-cost-climbing-stairs/min-cost-climbing-stairs.java
@@ -1,12 +1,15 @@
 class Solution {
     public int minCostClimbingStairs(int[] cost) {
-        int[] minCost = new int [cost.length + 1];
-        
-        for(int i=2; i<minCost.length; i++){
-            minCost[i] = Math.min(minCost[i-1] + cost[i-1], minCost[i-2] + cost[i-2]);
+        int[] minCost = new int[cost.length];
+        minCost[0] = cost[0];
+        minCost[1] = cost[1];
+
+        for(int i=2; i< cost.length; i++) {
+            minCost[i] = Math.min(cost[i] + minCost[i-1], cost[i] + minCost[i-2]);
         }
 
-        return minCost[minCost.length - 1];
+        // System.out.println(Arrays.toString(minCost));
 
+        return Math.min(minCost[minCost.length -1], minCost[minCost.length -2]);
     }
 }

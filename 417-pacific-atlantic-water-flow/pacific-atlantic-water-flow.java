3f2848046508ca4afee7d2ae885627a10b549b80
ManemDhanush <42865626+ManemDhanush@users.noreply.github.com>
2025-03-03 14:27:07 -0500

    Time: 4 ms (98.25%) | Memory: 46.1 MB (22.57%) - LeetSync

--- 417-pacific-atlantic-water-flow/pacific-atlantic-water-flow.java
+++ 417-pacific-atlantic-water-flow/pacific-atlantic-water-flow.java
@@ -6,13 +6,13 @@ class Solution {
         List<List<Integer>> ans = new ArrayList<>();
         
         for (int i = 0; i < heights.length; i++) {
-            visit(heights, pacific, heights[i][0], true, i, 0);
-            visit(heights, atlantic, heights[i][heights[0].length - 1], true, i, heights[0].length - 1);
+            visit(heights, pacific, heights[i][0], i, 0);
+            visit(heights, atlantic, heights[i][heights[0].length - 1], i, heights[0].length - 1);
         }
 
         for (int j = 0; j < heights[0].length; j++) {
-            visit(heights, pacific, heights[0][j], true, 0, j);
-            visit(heights, atlantic, heights[heights.length - 1][j], true, heights.length - 1, j);
+            visit(heights, pacific, heights[0][j], 0, j);
+            visit(heights, atlantic, heights[heights.length - 1][j], heights.length - 1, j);
         }
 
         // for(boolean[] d: pacific){
@@ -39,17 +39,17 @@ class Solution {
         return ans;
     }
 
-    void visit(int[][] heights, boolean[][] ocean, int val, boolean prevIndex, int i, int j) {
+    void visit(int[][] heights, boolean[][] ocean, int val, int i, int j) {
         if(i<0 || j<0 || i > heights.length - 1 || j > heights[0].length - 1) return;
         if(ocean[i][j]) return;
         if(heights[i][j] < val) return;
         
         ocean[i][j] = true;
 
-        visit(heights, ocean, heights[i][j], ocean[i][j], i+1, j);
-        visit(heights, ocean, heights[i][j], ocean[i][j], i, j+1);
-        visit(heights, ocean, heights[i][j], ocean[i][j], i-1, j);
-        visit(heights, ocean, heights[i][j], ocean[i][j], i, j-1);
+        visit(heights, ocean, heights[i][j], i+1, j);
+        visit(heights, ocean, heights[i][j], i, j+1);
+        visit(heights, ocean, heights[i][j], i-1, j);
+        visit(heights, ocean, heights[i][j], i, j-1);
 
     }
 

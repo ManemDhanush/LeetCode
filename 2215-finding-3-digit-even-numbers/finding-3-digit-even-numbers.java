5aa10052e5960ec6187ae2b09c7574155330b5a1
ManemDhanush <42865626+ManemDhanush@users.noreply.github.com>
2025-05-12 14:21:54 -0400

    Time: 309 ms (20.79%) | Memory: 45.5 MB (23.58%) - LeetSync

--- 2215-finding-3-digit-even-numbers/finding-3-digit-even-numbers.java
+++ 2215-finding-3-digit-even-numbers/finding-3-digit-even-numbers.java
@@ -3,30 +3,21 @@ import java.util.HashMap;
 
 class Solution {
     public int[] findEvenNumbers(int[] digits) {
-        ArrayList<Integer> ans = new ArrayList<>();
-        HashMap<Integer, Integer> map = new HashMap<>();
-
-        for (int digit : digits) {
-            map.put(digit, map.getOrDefault(digit, 0) + 1);
-        }
-
-        for (int num = 100; num <= 998; num += 2) { 
-            int hundreds = num / 100;
-            int tens = (num / 10) % 10;
-            int ones = num % 10;
-
-            HashMap<Integer, Integer> tempMap = new HashMap<>(map);
-            if (tempMap.getOrDefault(hundreds, 0) > 0) {
-                tempMap.put(hundreds, tempMap.get(hundreds) - 1);
-                if (tempMap.getOrDefault(tens, 0) > 0) {
-                    tempMap.put(tens, tempMap.get(tens) - 1);
-                    if (tempMap.getOrDefault(ones, 0) > 0) {
-                        ans.add(num);
+        Set<Integer> ans = new HashSet<>();
+        for(int i=0; i<digits.length; i++){
+            for(int j=0; j<digits.length; j++){
+                for(int k=0; k<digits.length; k++){
+                    if(i==j || j==k || k==i){
+                        continue;
+                    }
+                    // int temp = (i*100) + (j*10) + k;
+                    if(digits[k]%2 == 0 && digits[i]!=0){
+                        ans.add((digits[i]*100) + (digits[j]*10) + digits[k]);
                     }
                 }
             }
         }
 
-        return ans.stream().mapToInt(Integer::valueOf).toArray();
+        return ans.stream().mapToInt(Integer::valueOf).sorted().toArray();
     }
 }

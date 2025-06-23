c180858ba6ef3780908b4c62ef3a424b52e5582b
ManemDhanush <42865626+ManemDhanush@users.noreply.github.com>
2025-06-22 22:09:52 -0400

    Time: 1 ms (71.52%) | Memory: 42 MB (90.47%) - LeetSync

--- 199-binary-tree-right-side-view/binary-tree-right-side-view.java
+++ 199-binary-tree-right-side-view/binary-tree-right-side-view.java
@@ -15,31 +15,29 @@
  */
 class Solution {
 
-    Deque<TreeNode> list1 = new ArrayDeque<TreeNode>();
-    Deque<TreeNode> list2 = new ArrayDeque<TreeNode>();
-    List<Integer> ans = new ArrayList<Integer>();
-
     public List<Integer> rightSideView(TreeNode root) {
-        if(root!=null) list1.add(root);
-        bfs();
+        Queue<TreeNode> q = new LinkedList<>();
+        ArrayList<Integer> ans = new ArrayList<Integer>();
+        if (root == null) return ans;
+
+        q.offer(root);
+
+        while(!q.isEmpty()){
+            int size = q.size();
+            TreeNode right = null;
+
+            for(int i=0; i<size; i++){
+                TreeNode node = q.poll();
+                if (i == size - 1) {
+                    right = node;
+                }
+                if(node.left != null) q.offer(node.left);
+                if(node.right != null) q.offer(node.right);
+            }
+            ans.add(right.val);
+
+        }
         return ans;
     }
 
-    public void bfs() {
-        while(!list1.isEmpty()){
-            if(list1.isEmpty()) return;
-            TreeNode lastNode = list1.peekLast();
-            if (lastNode != null) {
-                ans.add(lastNode.val);
-            }
-
-            while(!list1.isEmpty()){
-                TreeNode node = list1.pollFirst();
-                if(node.left != null) list2.addLast(node.left);
-                if(node.right != null) list2.addLast(node.right);
-            }
-            list1.addAll(list2);
-            list2.clear();
-        }
-    }
 }

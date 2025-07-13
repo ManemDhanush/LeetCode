13f622cafec97c52834c728d13b02156d8892162
ManemDhanush <42865626+ManemDhanush@users.noreply.github.com>
2025-07-13 00:13:47 -0400

    Time: 1 ms (99.48%) | Memory: 44.7 MB (10.58%) - LeetSync

--- 443-string-compression/string-compression.java
+++ 443-string-compression/string-compression.java
@@ -2,27 +2,40 @@ class Solution {
     public int compress(char[] chars) {
         int num = 0;
         char prevChar = chars[0];
+        int idx = 0;
         StringBuilder ans = new StringBuilder();
 
         for(int i=0; i<chars.length; i++){
             if(prevChar != chars[i]){
-                ans.append(prevChar);
-                if(num!=1)
-                ans.append(num);
+                chars[idx++] = prevChar;
+                // ans.append(prevChar);
+                if(num!=1){
+                    String countStr = String.valueOf(num);
+                    for (char c : countStr.toCharArray()) {
+                        chars[idx++] = c;
+                    }
+                }
+                // ans.append(num);
                 num = 1;
                 prevChar = chars[i];
             } else {
                 num++;
             }
         }
-        ans.append(prevChar);
-        if(num!=1)
-        ans.append(num);
-
-        for(int i=0; i<ans.length(); i++){
-            chars[i] = ans.charAt(i);
+        // ans.append(prevChar);
+        chars[idx++] = prevChar;
+        if(num!=1){
+            String countStr = String.valueOf(num);
+            for (char c : countStr.toCharArray()) {
+                chars[idx++] = c;
+            }
         }
+        // ans.append(num);
 
-        return ans.length();
+        // for(int i=0; i<ans.length(); i++){
+        //     chars[i] = ans.charAt(i);
+        // }
+
+        return idx;
     }
 }

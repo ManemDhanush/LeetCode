0c6fc4393e7f48dcc1354c3c6cfa436b3201c962
ManemDhanush <42865626+ManemDhanush@users.noreply.github.com>
2026-01-08 16:40:11 -0800

    Time: 2 ms (98.93%) | Memory: 43 MB (78.65%) - LeetSync

--- 2464-time-needed-to-rearrange-a-binary-string/time-needed-to-rearrange-a-binary-string.java
+++ 2464-time-needed-to-rearrange-a-binary-string/time-needed-to-rearrange-a-binary-string.java
@@ -1,33 +1,29 @@
 class Solution {
     public int secondsToRemoveOccurrences(String s) {
-        int count = -1;
-        int[] bits = new int [s.length()];
-        int len = 0;
-        boolean isAns = false;
-        boolean isFirst = true;
-        for(char c: s.toCharArray()){
-            bits[len++] = Character.getNumericValue(c);
-        }
-
-        while(!isAns){
-            boolean swap = false;
-            for(int i=1; i<bits.length; i++){
-                if(bits[i-1] == 0 && bits[i] == 1){
-                    bits[i-1] = 1;
-                    bits[i] = 0;
-                    swap = true;
-                    i++;
+        int len = s.length();
+        int numOfOnes = 0;
+        int numOfZero = 0;
+        int firstZeroIndex = 0;
+        boolean changeZeroIndex = true;
+        int conZero = 0;
+        int ans = 0;
+        for(int i=0; i<len; i++){
+            if(s.charAt(i) == '0'){
+                numOfZero++;
+                 if(changeZeroIndex){
+                    changeZeroIndex = false;
+                    firstZeroIndex = i;
+                } else if(i - firstZeroIndex - conZero < 2){
+                    conZero++;
+                }
+            } else {
+                numOfOnes++;
+                if(numOfZero > 0){
+                    ans = Math.max(ans+1, numOfZero);
                 }
             }
-
-            if(!swap){
-                isAns = true;
-                // if(isFirst) return count;
-            }
-            isFirst = false;
-            count++;
         }
-        // System.out.println(Arrays.toString(bits));
-        return count;
+        if(numOfOnes + conZero == len) return 0;
+        return ans;
     }
 }

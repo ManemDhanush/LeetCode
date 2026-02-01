3fe1eedd47915c2560833502974e5f2b68173503
ManemDhanush <42865626+ManemDhanush@users.noreply.github.com>
2026-01-31 18:38:41 -0800

    Time: 52 ms (23.40%) | Memory: 134.2 MB (5.96%) - LeetSync

--- 146-lru-cache/lru-cache.java
+++ 146-lru-cache/lru-cache.java
@@ -1,70 +1,68 @@
 class LRUCache {
 
-    class DoublyLL {
+    class Node {
+        Node next;
+        Node prev;
+        int val;
         int key;
-        int value;
-        DoublyLL next, prev;
-        DoublyLL(int key, int value){
+
+        Node(int key, int val){
             this.key = key;
-            this.value = value;
+            this.val = val;
         }
     }
 
-    DoublyLL head;
-    DoublyLL tail;
-    HashMap<Integer, DoublyLL> map;
-    int capacity;
+    private int capacity;
+    private Node head;
+    private Node tail;
+    private HashMap<Integer, Node> map;
 
-    void add(DoublyLL node){
-        node.prev = tail.prev;
-        node.next = tail;
-        tail.prev.next = node;
-        tail.prev = node;
-    }
-
-    void remove(DoublyLL node){
-        node.prev.next = node.next;
-        node.next.prev = node.prev;
-        node.prev = null;
-        node.next = null;
-    }
 
     public LRUCache(int capacity) {
-        map = new HashMap<>();
         this.capacity = capacity;
-        head = new DoublyLL(-1, -1);
-        tail = new DoublyLL(-1, -1);
+        map = new HashMap<>();
+        head = new Node(-1, -1);
+        tail = new Node(-1, -1);
         head.next = tail;
         tail.prev = head;
     }
+
+    public void add(Node node){
+        Node nextNode = head.next;
+        node.prev = head;
+        node.next = nextNode;
+        nextNode.prev = node;
+        head.next = node;
+    }
+
+    public void remove(Node node){
+        node.prev.next = node.next; 
+        node.next.prev = node.prev;
+    }
     
     public int get(int key) {
-        DoublyLL x = map.getOrDefault(key, null);
-        if(x==null) return -1;
-        remove(x);
-        add(x);
-        return x.value;
+        if(map.containsKey(key)){
+            Node node = map.get(key);
+            remove(node);
+            add(node);
+            return node.val;
+        }
+        return -1;
     }
     
     public void put(int key, int value) {
-        DoublyLL x = map.get(key);
-        if (x != null) {
-            x.value = value;
-            remove(x);
-            add(x);
-            return;
+        if(map.containsKey(key)){
+            remove(map.get(key));
+        } else if(map.size() == capacity){
+            Node last = tail.prev;
+            remove(last);
+            map.remove(last.key);
         }
-
-        DoublyLL fresh = new DoublyLL(key, value);
-        add(fresh);
-        map.put(key, fresh);
-
-        if (map.size() > capacity) {
-            DoublyLL lru = head.next;
-            remove(lru);
-            map.remove(lru.key);
-        }
+        Node node = new Node(key, value);
+        add(node);
+        map.put(key, node);
     }
+
 }
 
 /**

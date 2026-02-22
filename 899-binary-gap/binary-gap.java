d055fd634d5abefed91e62d328e0f3f2f121df3c
ManemDhanush <42865626+ManemDhanush@users.noreply.github.com>
2026-02-22 11:47:32 -0800

    Time: 0 ms (100.00%) | Memory: 42.5 MB (29.57%) - LeetSync

--- 899-binary-gap/binary-gap.java
+++ 899-binary-gap/binary-gap.java
@@ -1,13 +1,17 @@
 class Solution {
     public int binaryGap(int n) {
-        int maxLen = 0;
-        int currIndex = 0;
-        String binaryString = Integer.toBinaryString(n);
-        for(int i=0; i<binaryString.length(); i++){
-            if(binaryString.charAt(i) == '0') continue;
-            maxLen = Math.max(maxLen, i - currIndex);
-            currIndex = i;
+        int[] bitMap = new int[32];
+        int index = 0;
+        for(int i=0; i<32; i++){
+            if(((n >> i) & 1) == 1){
+                bitMap[index] = i;
+                index++;
+            }
         }
-        return maxLen;
+
+        int ans = 0;
+        for (int i = 0; i < index - 1; ++i)
+            ans = Math.max(ans, bitMap[i+1] - bitMap[i]);
+        return ans;
     }
 }
